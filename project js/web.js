5e59cfa0a6c881d6f4386ccc110088b47bd76db8
keisidoci <keisidoci12@gmail.com>
2023-01-10 21:24:34 +0100

    First commit in GitHub

---
+++ project js/web.js
@@ -0,0 +1,32 @@
+function reserve() {
+    var email = document.getElementById("exampleFormControlInput1").value;
+    var note = document.getElementById("exampleFormControlTextarea1").value;
+
+    if (email == ""){
+        document.getElementById("mesazh_email").innerHTML = "Please fill your email!";
+        return false;
+    }
+    else {
+        window.alert("Meeting reserved!");
+        return true;
+    }
+}
+
+function application (){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("exampleInputEmail1").value;
+
+    if (!isNaN(name)) {
+        document.getElementById("mesazh_name").innerHTML = "Please enter your full name!";
+        return false;
+}
+    if (email == ""){
+    document.getElementById("mesazh_email").innerHTML = "Please fill your email!";
+        return false;
+
+} 
+else {
+    window.alert("Application is sent!");
+        return true;
+}
+}
